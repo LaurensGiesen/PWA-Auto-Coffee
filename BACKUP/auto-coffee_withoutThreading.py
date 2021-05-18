048c57fb2822b910ac0871e47b940bef9256e631
arthur.de.groote <arthur.de.groote@student.howest.be>
2021-05-18 12:01:58 +0200

    done

--- BACKUP/auto-coffee_withoutThreading.py
+++ BACKUP/auto-coffee_withoutThreading.py
@@ -7,22 +7,21 @@ import threading
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
-GPIO.cleanup()
 
-btnPairing = 12
-btnRelay1 = 13
-btnRelay2 = 15
-sensorReservoirPin = 11
-ledRedPin = 37
+btnPairing = 36 #oranje kabels!
+btnRelay1 = 31 #groene kabels!
+btnRelay2 = 33 #paarse kabels
+sensorReservoirPin = 32 #blauw
+ledPin = 35 #wit
 
 GPIO.setup(btnPairing, GPIO.IN, pull_up_down=GPIO.PUD_UP) #btnPairing
 GPIO.setup(btnRelay1,GPIO.IN, pull_up_down=GPIO.PUD_UP) #btnRelay1
 GPIO.setup(btnRelay2,GPIO.IN, pull_up_down=GPIO.PUD_UP) #btnRelay2
 
 GPIO.setup(sensorReservoirPin, GPIO.IN, pull_up_down=GPIO.PUD_UP) #sensorReservoir
-GPIO.setup(ledRedPin,GPIO.OUT) #ledRED
+GPIO.setup(ledPin,GPIO.OUT) #ledRED
 
-relayPins = [35,33,31,29]
+relayPins = [8,10] #8 : groen (btnrelay1) ; 10 : paars (btnrelay2)
 
 for i in relayPins: #set up all relayPins and turn them off
     GPIO.setup(i, GPIO.OUT)
@@ -37,7 +36,7 @@ db = firestore.client()
 
 coll_d = db.collection('Devices')
 
-Key = "1234"
+Key = '1234'
 pairing = False
 blinkspeed = 0.5
 
@@ -90,43 +89,62 @@ def button_pairing_callback(channel):
     elif pairing:
         pairing_end()
 
-def sensor_reservoir_callback():
+def sensor_reservoir_callback(channel):
     global reservoirFull
-    if GPIO.input(11) and reservoirFull: #Rising
+    if GPIO.input(sensorReservoirPin) and reservoirFull: #Rising
         print("Geen Water")
         coll_d.document(Key).update({u"reservoirFull":u"False"})
         reservoirFull = False
-    elif GPIO.input(11) != 1 and not reservoirFull: #falling
+    elif GPIO.input(sensorReservoirPin) != 1 and not reservoirFull: #falling
         print("Water")
         coll_d.document(Key).update({u"reservoirFull":u"True"})
         reservoirFull = True
 
 def button_relay1_callback(channel):
+    relay1_callback()
+
+def button_relay2_callback(channel):
+    relay2_callback()
+
+def relay1_callback():
     global relay1IsOn
     if not relay1IsOn:
         relayOn(relayPins[0])
         relay1IsOn = True
+        sleep(3)
+        coll_d.document(Key).set({u"Status": u"Available", "reservoirFull":False})
+        relayOf(relayPins[0])
+        relay1IsOn = False
     elif relay1IsOn:
         relayOf(relayPins[0])
         relay1IsOn = False
 
-def button_relay2_callback(channel):
-    global relay2IsOn
-    if not relay2IsOn:
+def relay2_callback():
+    global relay1IsOn
+    if not relay1IsOn:
         relayOn(relayPins[1])
         relay2IsOn = True
-    elif relay2IsOn:
+        sleep(3)
+        coll_d.document(Key).set({u"Status": u"Available", "reservoirFull":False})
+        relayOf(relayPins[1])
+        relay2IsOn = False
+    elif relay1IsOn:
         relayOf(relayPins[1])
         relay2IsOn = False
 
 def listen_status():
-    doc_ref = db.collection(u'Devices').document (Key)
+    doc_ref = db.collection(u'Devices').document(Key)
     doc = doc_ref.get()
     if doc.exists:
-        print(u'Document data:')
-        print(doc)
+        doc_dict = doc.to_dict()
     else:
         print(u'No such document!')
+    deviceStatus = doc_dict.get(u'Status')
+    if(deviceStatus == "One Cup"):
+        relay1_callback()
+    elif(deviceStatus == "Two Cup's"):
+        relay2_callback()
+
 
 GPIO.add_event_detect(btnPairing,GPIO.RISING,callback=button_pairing_callback)
 GPIO.add_event_detect(btnRelay1,GPIO.RISING,callback=button_relay1_callback)
@@ -139,18 +157,18 @@ init_device()
 try:
     
     while True:
-        on(37)
+        on(ledPin)
         sleep(blinkspeed)
-        of(37)
+        of(ledPin)
         sleep(blinkspeed)
         listen_status()
 except:
-    of(37)
+    of(ledPin)
     GPIO.cleanup()
     pairing_end()
     coll_d.document(Key).update({u"Status":u"offline"})
 finally:
-    of(37)
+    of(ledPin)
     GPIO.cleanup()
     pairing_end()
     coll_d.document(Key).update({u"Status":u"offline"})
